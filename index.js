4198b842d11fe164e2256935b9e4e11f470f2631
Kimia Darden <kimiadarden@gmail.com>
2020-02-29 23:14:07 -0500

    initial prompt

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require("inquirer");
-const connection = require('./db/connection');
+let Database = require("./db-index");
+let cTable = require("console.table");
+
+
+
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_DB"
+});
+
+
+
+async function initialPrompt() {
+    return inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do today?",
+                name: "action",
+                choices: [
+                  
+                  "View all departments",
+                  "View all employees",
+                  "View all roles",
+                  "Exit"
+                ]
+            }
+        ])
+}
